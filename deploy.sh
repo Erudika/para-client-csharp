b3ab663a2a86c9c04e91cf013826aee1a04966de
Alex Bogdanovski <alex@erudika.com>
2020-04-27 20:26:30 +0300

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+lastver=$(git describe --abbrev=0 --tags)
+echo "Last tag was: $lastver"
+echo "---"
 read -e -p "Tag: " ver
+
+sed -i -e "s/>$lastver</>$ver</g" para-client-csharp/para-client-csharp.csproj
+
 git add -A && git commit -m "Release v$ver."
 git tag "v$ver"
 git push origin master && git push --tags
